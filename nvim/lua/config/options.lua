d5468a13094c4d4ed1527443c90ac24e8aefb9d3
shyhsheng <shyhsheng@gmail.com>
2026-04-29 22:11:02 +0800

    feat(nvim): add options from previous nvim settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+-- vim.opt.tabstop = 8
+-- vim.opt.shiftwidth = 8
+-- vim.opt.softtabstop = 8
+-- vim.opt.expandtab = false
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+
+vim.api.nvim_set_hl(0, "@lsp.mod.inactive", { link = "Normal" })
