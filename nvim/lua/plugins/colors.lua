7755a7e00b5cc92fe60188b05e2fc95f803cd4fa
shyhsheng <shyhsheng@gmail.com>
2026-04-29 22:11:02 +0800

    refactor(nvim): indent code of all plugins

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      on_colors = function(colors)
-        -- 可選：如果你想改 palette
-      end,
-      on_highlights = function(hl, c)
-        --  Visual
-        hl.Visual = { bg = "#3d59a1" }
+    {
+        "folke/tokyonight.nvim",
+        opts = {
+            on_colors = function(colors)
+                -- 可選：如果你想改 palette
+            end,
+            on_highlights = function(hl, c)
+                --  Visual
+                hl.Visual = { bg = "#3d59a1" }
 
-        --  Illuminate
-        hl.IlluminatedWordText = { bg = "#545c8c", underline = true }
-        hl.IlluminatedWordRead = { bg = "#545c8c", underline = true }
-        hl.IlluminatedWordWrite = { bg = "#545c8c", underline = true }
-      end,
+                --  Illuminate
+                hl.IlluminatedWordText = { bg = "#545c8c", underline = true }
+                hl.IlluminatedWordRead = { bg = "#545c8c", underline = true }
+                hl.IlluminatedWordWrite = { bg = "#545c8c", underline = true }
+            end,
+        },
     },
-  },
 }
--- nvim/lua/plugins/interestingwords.lua
+++ nvim/lua/plugins/interestingwords.lua
@@ -11,30 +11,30 @@ return {
     event = "VeryLazy",
     config = function()
         require("interestingwords").setup {
-        colors = {
+            colors = {
                 '#9ece6a', -- green (success / normal)
                 '#e0af68', -- yellow (warning)
                 '#7aa2f7', -- blue (info)
                 '#bb9af7', -- purple (secondary)
                 '#2ac3de', -- cyan (highlight)
                 '#f7768e' -- red-pink (critical)
-        },
-        -- colors = {
-        --   '#00ff00', -- neon green
-        --   '#ffff00', -- neon yellow
-        --   '#00ffff', -- cyan
-        --   '#ff00ff', -- magenta
-        --   '#ff8800', -- orange
-        --   '#ff0055', -- strong red
-        -- }
-        search_count = true,
-        navigation = false,
-        scroll_center = true,
-        search_key = "<leader>m",
-        cancel_search_key = "<leader>M",
-        color_key = "<leader>k",
-        cancel_color_key = "<leader>K",
-        select_mode = "loop",  -- random or loop
-    }
+            },
+            -- colors = {
+            --   '#00ff00', -- neon green
+            --   '#ffff00', -- neon yellow
+            --   '#00ffff', -- cyan
+            --   '#ff00ff', -- magenta
+            --   '#ff8800', -- orange
+            --   '#ff0055', -- strong red
+            -- }
+            search_count = true,
+            navigation = false,
+            scroll_center = true,
+            search_key = "<leader>m",
+            cancel_search_key = "<leader>M",
+            color_key = "<leader>k",
+            cancel_color_key = "<leader>K",
+            select_mode = "loop",  -- random or loop
+        }
     end,
 }
