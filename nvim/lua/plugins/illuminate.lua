4c4cb9bb090fb1821c9ce278c629eff8fcf683a8
shyhsheng <shyhsheng@gmail.com>
2026-04-30 09:37:19 +0800

    feat(nvim): enable illuminate in LazyExtra

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -2,17 +2,12 @@
 return {
     {
         "RRethy/vim-illuminate",
-        event = "VeryLazy",
-        config = function()
-            require("illuminate").configure({
-                delay = 200,
-                under_cursor = true,
-                providers = {
-                    "lsp",
-                    "treesitter",
-                    "regex",
-                },
-            })
-        end,
+        opts = {
+            under_cursor = true,
+            providers = {
+                "lsp",
+                "treesitter",
+            },
+        },
     },
 }
