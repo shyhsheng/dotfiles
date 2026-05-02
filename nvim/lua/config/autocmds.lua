d4a721c4828710c348f471be14baf5f178f84ca9
shyhsheng <shyhsheng@gmail.com>
2026-05-02 11:32:43 +0800

    feat(nvim): focus dashboard on startup when opening directory

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,22 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+            vim.schedule(function()
+                -- back to dashboard / startup buffer
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    local buf = vim.api.nvim_win_get_buf(win)
+                    local ft = vim.bo[buf].filetype
+
+                    if ft == "snacks_dashboard" or ft == "dashboard" or ft == "alpha" then
+                        vim.api.nvim_set_current_win(win)
+                        return
+                    end
+                end
+            end)
+        end
+    end,
+})
